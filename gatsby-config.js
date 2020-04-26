558c1079c62ede5b8cab781f1958e2c77024ea21
Pat-Lim <impatlim@gmail.com>
2020-04-27 10:01:07 +1200

    complete W09

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Pat's Gatsby website`,
+    description: "Trying out making a static site with Gatsby.js",
+    author: `Pat Lim`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,13 +2,16 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Image from "../components/image"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Thanks for coming!</h1>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
